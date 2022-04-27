767347c2d05030a9b8a5a68225431c852061dc25
OPTIC <68736438+Brnjak6@users.noreply.github.com>
2022-04-27 23:26:23 +0100

    fixing hardcoded outdated data

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,44 +1,44 @@
 export const enrichingSet = [
   {
     id: 642136027,
-    name: "Mini Bar",
+    name: 'Mini Bar',
     image:
-      "https://i.etsystatic.com/17518176/r/il/243312/3154434473/il_fullxfull.3154434473_4qjk.jpg",
-    price: "3672.98",
+      'https://i.etsystatic.com/17518176/r/il/243312/3154434473/il_fullxfull.3154434473_4qjk.jpg',
+    price: '3672.98',
   },
   {
     id: 947735735,
-    name: "Room Lights",
+    name: 'Room Lights',
     image:
-      "https://i.etsystatic.com/23093901/r/il/ee6488/2814477750/il_794xN.2814477750_20w4.jpg",
-    price: "17.95",
+      'https://i.etsystatic.com/23093901/r/il/ee6488/2814477750/il_794xN.2814477750_20w4.jpg',
+    price: '17.95',
   },
   {
     id: 949303486,
-    name: "Soft Carpet",
+    name: 'Soft Carpet',
     image:
-      "https://i.etsystatic.com/24643069/r/il/edbfe8/2921263565/il_fullxfull.2921263565_f3ok.jpg",
-    price: "178.00",
+      'https://i.etsystatic.com/24643069/r/il/edbfe8/2921263565/il_fullxfull.2921263565_f3ok.jpg',
+    price: '178.00',
   },
   {
     id: 944129826,
-    name: "Pouf Squares",
+    name: 'Pouf Squares',
     image:
-      "https://i.etsystatic.com/13796739/r/il/144b55/2872400368/il_fullxfull.2872400368_a419.jpg",
-    price: "129.15",
+      'https://i.etsystatic.com/13796739/r/il/144b55/2872400368/il_fullxfull.2872400368_a419.jpg',
+    price: '129.15',
   },
   {
     id: 971370843,
-    name: "Mini Table",
+    name: 'Mini Table',
     image:
-      "https://i.etsystatic.com/18197369/r/il/704146/1572622756/il_fullxfull.1572622756_6kn0.jpg",
-    price: "67.50",
+      'https://i.etsystatic.com/18197369/r/il/704146/1572622756/il_fullxfull.1572622756_6kn0.jpg',
+    price: '67.50',
   },
   {
-    id: 880690644,
-    name: "Vintage Table",
+    id: 557344113,
+    name: 'Reclaimed Dining Table',
     image:
-      "https://i.etsystatic.com/25760710/r/il/6e42f0/2668549561/il_fullxfull.2668549561_l4jh.jpg",
-    price: "249.00",
+      'https://i.etsystatic.com/15845989/r/il/f35fe9/2022512305/il_1140xN.2022512305_rrhq.jpg',
+    price: '258.38',
   },
 ];
